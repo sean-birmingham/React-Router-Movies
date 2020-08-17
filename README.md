55e911939cc4f3b1b57ed574ade1dd98714e3664
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-17 14:30:43 -0400

    set up routes

--- README.md
+++ README.md
@@ -26,10 +26,10 @@ Topics:
 
 #### Set up Routes
 
-- [ ] Wrap your app with the router.
-- [ ] Inside your App file add two routes.
-  - [ ] one route for `/` that loads the `MovieList` component. **This component will need the movies injected into it via props**.
-  - [ ] one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component.
+- [x] Wrap your app with the router.
+- [x] Inside your App file add two routes.
+  - [x] one route for `/` that loads the `MovieList` component. **This component will need the movies injected into it via props**.
+  - [x] one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component.
 
 #### Add Functionality
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Switch, Route } from 'react-router-dom';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -11,24 +14,37 @@ const App = () => {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies')
-        .then(response => {
+        .then((response) => {
           setMovieList(response.data);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Server Error', error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
+      <Switch>
+        <Route exact path='/'>
+          <MovieList movies={movieList} />
+        </Route>
+        <Route path='/movies/:id'>
+          <Movie movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 };
